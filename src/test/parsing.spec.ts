150f24c6e5f88602575596a3402ee4764da81cfd
mloetkemann <m.loetkemann@gmail.com>
2023-05-09 21:59:49 +0200

    feat: parsing methods

---
+++ src/test/parsing.spec.ts
@@ -0,0 +1,74 @@
+import { assert } from 'chai'
+import {parseToNumber, parseToObject, parseToString} from '../lib/parsing'
+
+describe('parsing', () => {
+
+  it('parse string', async () => {
+    const val = "Test"
+    let anyValue: unknown
+    // eslint-disable-next-line prefer-const
+    anyValue = val
+
+    const parsedValue = parseToString(anyValue)
+    if(parsedValue)
+      assert.equal(parsedValue, val)
+    else
+      assert.fail("Value undefined")
+  })
+
+  it('parse string undefined', async () => {
+    const val = 100
+    let anyValue: unknown
+    // eslint-disable-next-line prefer-const
+    anyValue = val
+
+    const parsedValue = parseToString(anyValue)
+    assert.isUndefined(parsedValue)
+  })
+
+  it('parse number', async () => {
+    const val = 100
+    let anyValue: unknown
+    // eslint-disable-next-line prefer-const
+    anyValue = val
+
+    const parsedValue = parseToNumber(anyValue)
+    if(parsedValue)
+      assert.equal(parsedValue, val)
+    else
+      assert.fail("Value undefined")
+  })
+
+  it('parse number undefined', async () => {
+    const val = "string"
+    let anyValue: unknown
+    // eslint-disable-next-line prefer-const
+    anyValue = val
+
+    const parsedValue = parseToNumber(anyValue)
+    assert.isUndefined(parsedValue)
+  })
+
+  it('parse object', async () => {
+    const val = { prop: "value"}
+    let anyValue: unknown
+    // eslint-disable-next-line prefer-const
+    anyValue = val
+
+    const parsedValue = parseToObject(anyValue)
+    if(parsedValue)
+      assert.equal(parsedValue, val)
+    else
+      assert.fail("Value undefined")
+  })
+
+  it('parse object null', async () => {
+    const val = "string"
+    let anyValue: unknown
+    // eslint-disable-next-line prefer-const
+    anyValue = val
+
+    const parsedValue = parseToObject(anyValue)
+    assert.isNull(parsedValue)
+  })
+})
