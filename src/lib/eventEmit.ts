e396500ae0398b9bc4282f5e6855624dfa704e6c
mloetkemann <m.loetkemann@gmail.com>
2023-09-28 21:47:42 +0200

    feat: rabbitmq message broker

--- src/lib/eventEmit.ts
+++ src/lib/eventEmit.ts
@@ -1,3 +1,8 @@
+import amqp from 'amqplib'
+import 'dotenv/config'
+
+const ENV_EVENTEMITTER_DRIVER = 'npm_package_alpha8Config_eventDriver'
+
 export type EventListenerFunc = (param: EventParameter) => void
 export class EventParameter extends Map<string, unknown> {
   getString(key: string): string | undefined {
@@ -17,21 +22,30 @@ export default class EventEmit {
   private events = new Map<string, Array<EventListenerFunc>>()
   private static inst: EventEmit
 
-  public static getEmitter() {
-    if (!EventEmit.inst) EventEmit.inst = new EventEmit()
+  public static async getEmitter(): Promise<EventEmit> {
+    const driver = process.env[ENV_EVENTEMITTER_DRIVER]
+    if (!EventEmit.inst) {
+      if (driver === 'rabbitmq') {
+        EventEmit.inst = new RabbitMQEventEmitter()
+      } else {
+        EventEmit.inst = new EventEmit()
+      }
+
+      await EventEmit.inst.init()
+    }
     return EventEmit.inst
   }
 
-  private constructor() {
-    this.registerEvent('__all') // Event which is triggered if any other event is triggerd
-    this.registerEvent('__log') // Log Event which is triggered if any other event is triggerd
+  protected async init() {
+    //await this.registerEvent('__all') // Event which is triggered if any other event is triggerd
+    //await this.registerEvent('__log') // Log Event which is triggered if any other event is triggerd
   }
 
-  public registerEvents(events: Array<string>) {
-    events.forEach(event => this.registerEvent(event))
+  public async registerEvents(events: Array<string>) {
+    return Promise.all(events.map(event => this.registerEvent(event)))
   }
 
-  public registerEvent(event: string) {
+  public async registerEvent(event: string) {
     if (!this.events.has(event)) {
       this.events.set(event, new Array<EventListenerFunc>())
     } else {
@@ -47,22 +61,14 @@ export default class EventEmit {
   }
 
   async trigger(event: string, parameter?: object) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    let parameterEntries = new Array<[string, any]>()
-    if (parameter) {
-      parameterEntries = Object.entries(parameter)
-    }
+    this.triggerEvent('__all', parameter)
 
-    const param = new EventParameter(parameterEntries)
+    await this.triggerEvent(event, parameter)
 
-    this.triggerEvent('__all', param)
-
-    await this.triggerEvent(event, param)
-
-    this.addParameter(param, '__message', `Event ${event} triggered`)
-    this.addParameter(param, '__logLevel', `info`)
-
-    this.triggerEvent('__log', param)
+    this.triggerEvent('__log', {
+      __message: `Event ${event} triggered`,
+      __loglevel: 'info',
+    })
   }
 
   private addParameter(param: EventParameter, key: string, value: unknown) {
@@ -72,12 +78,87 @@ export default class EventEmit {
     }
   }
 
-  private async triggerEvent(event: string, param: EventParameter) {
+  protected triggerEvent(event: string, parameter?: object) {
+    this.notify(event, parameter)
+  }
+
+  protected async notify(event: string, parameter?: object) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let parameterEntries = new Array<[string, any]>()
+    if (parameter) {
+      parameterEntries = Object.entries(parameter)
+    }
+    const param = new EventParameter(parameterEntries)
     const eventListener = this.events.get(event)
     if (eventListener) {
       eventListener.forEach(listener => listener(param))
     }
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public async close() {}
 }
 
-EventEmit.getEmitter()
+class RabbitMQEventEmitter extends EventEmit {
+  private channel: amqp.Channel | undefined
+  private connection: amqp.Connection | undefined
+  async connectQueue(): Promise<[amqp.Connection, amqp.Channel] | undefined> {
+    try {
+      const connection = await amqp.connect('amqp://192.168.99.100:5672')
+      const channel = await connection.createChannel()
+      return [connection, channel]
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  constructor() {
+    super()
+  }
+
+  protected async init() {
+    await super.init()
+    return this.connectQueue().then(res => {
+      if (res) {
+        this.connection = res[0]
+        this.channel = res[1]
+
+        process.on('SIGINT', function () {
+          res[1].close()
+          res[0].close()
+          process.exit()
+        })
+      }
+    })
+  }
+  public async registerEvent(event: string) {
+    await super.registerEvent(event)
+    await this.channel?.assertQueue(event, { durable: false })
+    await this.channel?.consume(
+      event,
+      msg => {
+        if (msg) {
+          const content = Buffer.from(msg.content).toString()
+          this.notify(event, JSON.parse(content))
+        }
+      },
+      {
+        noAck: true,
+      }
+    )
+  }
+  protected triggerEvent(event: string, parameter?: object | undefined): void {
+    this.sendData(event, parameter)
+  }
+
+  async sendData(event: string, data?: object) {
+    await this.channel?.assertQueue(event, { durable: false })
+    await this.channel?.sendToQueue(event, Buffer.from(JSON.stringify(data)))
+  }
+
+  public async close() {
+    await this.channel?.close()
+    await this.connection?.close()
+
+    console.log('Closed')
+  }
+}
